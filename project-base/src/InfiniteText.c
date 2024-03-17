6589b31159400c162da8773ba114d26f12f3fadb
os-vm <lukajane23@gmail.com>
2024-03-17 09:25:13 -0700

    InfiniteText.c

---
+++ project-base/src/InfiniteText.c
@@ -0,0 +1,19 @@
+#include "../lib/console.h"
+#include "../lib/hw.h"
+
+uint64 pc;
+
+void a(){
+    asm volatile("mv %0, ra" : "=r" (pc));
+}
+
+void b(){
+    asm volatile("mv ra, %0" : : "r" (pc));
+}
+
+void main(){
+    a();
+    __putc('a');
+    __putc('\n');
+    b();
+}
